c0a42fb1f3d7c7f6102cc7e361fdd38ad9f65f04
ayushisinghal9 <ayushisinghal591@gmail.coml>
2020-08-17 15:10:27 +0530

    array of integer

---
+++ array1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[10],n,sum=0;
+	cout<<"enter size of array";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cout<<"enter element"<<i+1;
+		cin>>a[i];
+		sum=sum+a[i];
+	}
+	cout<<"array sum is: "<<sum;
+}
